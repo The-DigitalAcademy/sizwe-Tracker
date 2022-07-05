001cb8cddfd6fbd4e31b12d92e000a0b2cb184c1
unknown <sizwenqwala@gmail.com>
2022-07-05 11:13:44 +0200

    added balanceEl,incomeEl,expenseEl,transactionEl

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var state = {
                 balance: 0,
                 income: 0,
-                expense: 0;
-                transaction: 
-              
-            }
+                expense: 0,
+                transaction: [
+                     {name:'', amount: 0, type: 'income'},  
+                     {name:'', amount: 0, type: 'expense'},         
+                ]
+}
+
+var balanceEl = document.getElementById('balance');
+var incomeEl = document.getElementById('income');
+var expenseEl = document.getElementById('expense');
+var transactionEl = document.getElementById('transaction');
