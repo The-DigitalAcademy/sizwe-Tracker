eacf90aecca3fe905bc16e0affb3e6b8290b24f7
unknown <sizwenqwala@gmail.com>
2022-07-06 09:38:24 +0200

    added button expense, income

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var state = {
                 balance: 0,
                 income: 0,
                 expense: 0,
-                transaction: [
-                     {name:'', amount: 0, type: 'income'},  
-                     {name:'', amount: 0, type: 'expense'},         
+                history: [
+                     {name:'name', amount: 0, type: 'income'},  
+                     {name:'name', amount: 0, type: 'expense'},         
                 ]
 }
 
-var balanceEl = document.getElementById('#balance');
+var balanceEl = document.getElementById('balance');
 var incomeEl = document.getElementById('income');
 var expenseEl = document.getElementById('expense');
 var transactionEl = document.getElementById('transaction');
