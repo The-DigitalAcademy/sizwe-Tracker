45ef46e0394eefa5d00fa2df842a8f91368c5e17
unknown <sizwenqwala@gmail.com>
2022-07-22 13:01:12 +0200

    added const  budgetIncomeValue

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const budgetValue = document.querySelector(".budget-value");
 const budgetIT = document.querySelector(".budget-it");
 const budgetET = document.querySelector("budget-et");
 const budget = document.querySelector("#budget");
+const budgetIncomeValue = document.querySelector(".budget-income-value");
