f342f7fe6b03dc70cb10d928447fcbef07abc267
unknown <sizwenqwala@gmail.com>
2022-07-22 13:36:59 +0200

    added function addToList

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ const incomeLi = document.querySelector(".income-li");
 const expenseLi = document.querySelector(".expense-li");
 const deleteButton = document.querySelector(".delete-button");
 const newBudget = document.querySelector("#new-budget");
+
+var total = [];
+var index_number = 0;
+var dS = "$";
+var income = 0;
+var expense = 0;
+var amount_left = 0;
+
+addButton.addEventListener("click", function(){addToList(null);});
+
+function addToList(selection) {
+  event.preventDefault();
+  selectionOption = document.getElementById("new-select").value;
+  if ((selectionOption == "new-income" && selection == null) || selection == "newIncome");
+}
