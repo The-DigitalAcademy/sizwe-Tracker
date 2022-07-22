de55b04333e5bd4ce8e0b040e47584c414f0c96b
unknown <sizwenqwala@gmail.com>
2022-07-22 12:46:53 +0200

    added budget

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const budgetValue = document.querySelector(".budget-value");
 const budgetIT = document.querySelector(".budget-it");
+const budgetET = document.querySelector("budget-et");
+const budget = document.querySelector("#budget");
